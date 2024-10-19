9b76e77cde90e240c641563e8bbf9055cfcf333a
DESKTOP-7S31SVB\Fix-Dell <mohammedadell301@gmail.com>
2024-10-19 19:49:40 +0300

    added footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.top-seller-section .item').hover(function() {
+    $('.top-seller-section .item , .recently-section .item').hover(function() {
         const $images = $(this).find('.img img');
         let index = 0;
 
@@ -11,7 +11,7 @@ $(document).ready(function() {
         };
 
         // Start interval for changing opacity
-        const interval = setInterval(changeOpacity, 2000); // Change image every 2 seconds
+        const interval = setInterval(changeOpacity, 1000); // Change image every 2 seconds
 
         $(this).data('interval', interval); // Store interval ID for clearing later
     }, function() {
@@ -19,5 +19,35 @@ $(document).ready(function() {
         clearInterval($(this).data('interval')); // Clear the interval
         $images.css('opacity', 0); // Hide all images
         $images.first().css('opacity', 1); // Show the first image again
+    });
+    var swiper = new Swiper('.recently-section .products-slider .slider', {
+        slidesPerView: 5,
+        spaceBetween: 24,
+        speed: 1500,
+        navigation: {
+            nextEl: '.products-slider .swiper-button-next',
+            prevEl: '.products-slider .swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000,
+        },
+        loop: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            480: {
+                slidesPerView: 2,
+            },
+            787: {
+                slidesPerView: 3,
+            },
+            991: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 5,
+            }
+        }
     });
 });
